4f7cbc007442ac41e9e0db150fed167f991344c9
Gerie-2017 <gerie@kangwon.ac.kr>
2023-11-24 16:17:19 +0900

    func completed

--- mine.py
+++ mine.py
@@ -1,15 +1,18 @@
 def compute_ints(a=None,b=None):
-    a = input("Enter a number: ", int(a))
+    if a is None:
+        a = int(input("Enter a number: "))
+    if b is None:
+        b = int(input("Enter another number: "))
 
-    b = input("Enter another number: ", int(b))
-
-    operator = input("Enter an operator: type among +, -, *, /, %, **:")
+    operator = input("Enter an operator: type among (+, -, *, /, %, **):")
 
     try:
-        if b==0: 
-            print("Error: I cannot accept a zero value for the second number.")
+        if b==0 and operator in ("/", "%"):
+            print("Error: Cannot divide by zero.")
+            return None
     except ValueError:
         print("Error: I cannot accept a zero value for the second number.")
+        return None
 
     if operator == "+":
         result = (a+b)
@@ -23,4 +26,9 @@ def compute_ints(a=None,b=None):
         result=(a%b)
     elif operator == "**":
         result=(a**b)
+    else:
+        print("Error: Invalid operator.")
+        return None
     return result
+result = compute_ints()
+print(result)
