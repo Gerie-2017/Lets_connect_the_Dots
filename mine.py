20a42f697defc56e8a989cfb01fb5bde88b0857b
Gerie-2017 <gerie@kangwon.ac.kr>
2023-11-24 15:52:16 +0900

    python file of mine

---
+++ mine.py
@@ -0,0 +1,28 @@
+def compute_ints(a,b):
+    a = input("Enter a number: ", int(a))
+
+    b = input("Enter another number: ", int(b))
+
+    operator = input("Enter an operator: type among +, -, *, /, %, **:")
+
+    try:
+        if b==0: 
+            print("Error: I cannot accept a zero value for the second number.")
+    except ValueError:
+        print("Error: I cannot accept a zero value for the second number.")
+
+
+
+    if operator == "+":
+        result = (a+b)
+    elif operator == "-":
+        result= (a-b)
+    elif operator == "*":
+        result=(a*b)
+    elif operator == "/":
+        result=(a/b)
+    elif operator == "%":
+        result=(a%b)
+    elif operator == "**":
+        result=(a**b)
+    return result
